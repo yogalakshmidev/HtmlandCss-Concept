ea4b2b788acfaf206c210231f6a3a5d7453b0960
Yogalakshmi <yogarajakanibe@gmail.com>
2024-09-29 08:09:18 +0530

    BasicExamples-MiniTask3 added

---
+++ GuviSeptember/js-basics/datatypes.js
@@ -0,0 +1,6 @@
+let x = 100; //number max range2^53-1
+let name = "sanjay"; //string
+let isIndian = true; //boolean
+let z; //undefined
+let bio = null; //null
+let bigNum = 99999999999999999999n; //bigInt
